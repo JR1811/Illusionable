e76ca7ddc0a04c60e017685e95e8f332e788bb33
JR1811 <janreichelt77@gmail.com>
2025-03-15 03:27:56 +0100

    added improved item functionality

--- src/main/java/net/shirojr/illusionable/item/ObfuscatedDebugItem.java
+++ src/main/java/net/shirojr/illusionable/item/ObfuscatedDebugItem.java
@@ -1,5 +1,6 @@
 package net.shirojr.illusionable.item;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -7,6 +8,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -20,17 +22,32 @@ public class ObfuscatedDebugItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack stack = user.getStackInHand(hand);
+        if (toggleObfuscation(world, user, user, stack)) return TypedActionResult.success(stack, world.isClient());
+        return TypedActionResult.pass(stack);
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        if (toggleObfuscation(user.getWorld(), user, entity, stack)) {
+            return ActionResult.success(user.getWorld().isClient());
+        }
+        return ActionResult.PASS;
+    }
+
+    public static boolean toggleObfuscation(World world, PlayerEntity user, LivingEntity target, ItemStack stack) {
+        if (user.getItemCooldownManager().isCoolingDown(stack.getItem())) return false;
         if (world instanceof ServerWorld serverWorld) {
-            if (!user.hasStatusEffect(IllusionableStatusEffects.OBFUSCATED)) {
-                user.addStatusEffect(new StatusEffectInstance(IllusionableStatusEffects.OBFUSCATED, 20 * 60 * 5));
+            if (!target.hasStatusEffect(IllusionableStatusEffects.OBFUSCATED)) {
+                target.removeStatusEffect(IllusionableStatusEffects.OBFUSCATED);
+                target.addStatusEffect(new StatusEffectInstance(IllusionableStatusEffects.OBFUSCATED, 20 * 60 * 5));
                 serverWorld.playSound(null, user.getBlockPos(), SoundEvents.ENTITY_LEASH_KNOT_PLACE, SoundCategory.PLAYERS, 2f, 1f);
                 user.getItemCooldownManager().set(stack.getItem(), 100);
             } else {
-                user.removeStatusEffect(IllusionableStatusEffects.OBFUSCATED);
+                target.removeStatusEffect(IllusionableStatusEffects.OBFUSCATED);
                 serverWorld.playSound(null, user.getBlockPos(), SoundEvents.ENTITY_LEASH_KNOT_BREAK, SoundCategory.PLAYERS, 2f, 1f);
                 user.getItemCooldownManager().set(stack.getItem(), 100);
             }
         }
-        return TypedActionResult.success(stack, world.isClient());
+        return true;
     }
 }
